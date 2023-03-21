fc5930f699ff159fac6f953d22a8d4525a769e27
Akhil <akhil29122003penakacherla@gmail.com>
2023-03-21 18:57:59 -0400

    fixed checkbox issue

--- main.js
+++ main.js
@@ -16,8 +16,6 @@ const todoForm = document.querySelector("#new-todo");
 const todoName = document.querySelector("#content");
 const add_btn = document.getElementById("createbutton");
 
-add_btn.addEventListener("submit", addItem);
-
 // DOM Elements
 let appContainer = document.getElementById(appID);
 
@@ -29,7 +27,10 @@ function addItem(e) {
   e.preventDefault();
   const todo = e.target.elements.content.value;
   console.log("hi i am here at addItem", todos);
-  todos.push(todo);
+  todos.push({
+    text: todo,
+    isdone: false,
+  });
   DisplayTodos(todos,todoList);
   todoForm.reset();
 }
@@ -50,15 +51,13 @@ function addItem(e) {
     </div>
   </li> */
 
-
-
 function DisplayTodos(items, itemsList) {
 
   while (itemsList.firstChild) {
     itemsList.removeChild(itemsList.firstChild);
   }
   
-  console.log("hi i am here at DisplayTodos", todos);
+  console.log("hi i am here at DisplayTodos before", todos);
 
   for (let i = 0; i < items.length; i++) {
     const todoItem = document.createElement("li");
@@ -71,24 +70,54 @@ function DisplayTodos(items, itemsList) {
     const deletebutton = document.createElement("button");
     const contentdisplay = document.createElement("button");
 
-
     todoItem.classList.add("todo-item");
     content.classList.add("todo-content");
     actions.classList.add("action");
     input.type="checkbox";
     input.id="checkbox";
+    input.dataset.indexcheckbox = i;
+    input.checked = todos[i].isdone;
+    
+    if(input.checked){
+      todoItem.classList.add("done");
+    }
+
     span.classList.add("bubble");
+
     deletebutton.classList.add("deletebutton");
-    deletebutton.id="delete";
+    deletebutton.id="deletebutton";
     deletebutton.button="button";
     deletebutton.textContent = "Delete";
+    deletebutton.dataset.index = i;
+
+    deletebutton.addEventListener("click", (event)=>{
+      //console.log("Im a button");
+      // console.log(event.target.dataset.index);
+
+      todos.splice(event.target.dataset.index,1);
+
+      DisplayTodos(todos,todoList);
+    });
+
+    /*input.addEventListener("change", (event)=> {
+      console.log("Im a checkbox");
+      console.log(event.target.dataset.indexcheckbox);
+      
+      if(input.checked) {
+        todoItem.classList.add("done");
+      } else {
+        todoItem.classList.remove("done");
+      }
+
+      DisplayTodos(todos,todoList);
+    }); */
 
     todoItem.appendChild(label);
     todoItem.appendChild(content);
     todoItem.appendChild(actions);
 
     content.appendChild(contentdisplay);
-    contentdisplay.textContent = todos[i];
+    contentdisplay.textContent = todos[i].text;
 
     label.appendChild(input);
     label.appendChild(span);
@@ -96,50 +125,45 @@ function DisplayTodos(items, itemsList) {
     actions.appendChild(deletebutton);
     itemsList.appendChild(todoItem);
 
-    let checkbox = document.getElementById("checkbox");
-    checkbox.addEventListener("change", () =>{
-      if(input.checked) {
-        todoItem.classList.add("done");
+    console.log("hi i am here at DisplayTodos after", todos);
+  
+    input.addEventListener("click", (event) =>{
+      console.log(input.dataset.indexcheckbox);
+      // todos[event.target.dataset.indexcheckbox].isdone = input.checked;
+      console.log(todos[event.target.dataset.indexcheckbox].isdone, input.checked);
+      
+      if(input.checked){
+        console.log("fired");
+        //todoItem.classList.add("done");
+        todos[event.target.dataset.indexcheckbox].isdone = true;
       } else {
-        todoItem.classList.remove("done");
+        //todoItem.classList.remove("done");
+        todos[event.target.dataset.indexcheckbox].isdone = false;
       }
-      DisplayTodos();
+
+      // if(todos[event.target.dataset.indexcheckbox].isdone){
+      //   todoItem.classList.add("done");
+      // } else {
+      //   todoItem.classList.remove("done");
+      // }
+
+      console.log(todoItem);
+      // todoItem.classList.add("done");
+      DisplayTodos(todos,todoList);
     })
-
-    let deletelist = document.getElementById("delete");
-    deletelist.addEventListener("click", () =>{
-      todos.splice(i,1);
-      DisplayTodos();
-    })
-}
-  /*
-    if(todo.done){
-      todoItem.classList.add("done");
-    }
-
-    console.log("hi i am here at DisplayTodos", todos);
-
-    input.addEventListener("click", e=>{
-      todo.done = e.target.checked;
-
-      if(todo.done) {
-        todoItem.classList.add("done");
-      } else {
-        todoItem.classList.remove("done");
-      }
-      console.log("hi i am here at DisplayTodos", todos);
-      DisplayTodos();
-    })
-
-    deletebutton.addEventListener("click", e=>{
-      todos = todos.filter(t => t != todo);
-      DisplayTodos();
-      })
-  }) */
+}    
 }
 
 //
 // Inits & Event Listeners
 //
-todoForm.addEventListener("submit",addItem);
-addEventListener("click", listClickHander);
+todoForm.addEventListener("submit", addItem);
+//todoList.addEventListener("click", handleButtonCickINsideUl);
+
+/*
+
+Delete All
+todos.length = 0;
+renderlist;
+
+*/
